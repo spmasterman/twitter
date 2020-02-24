99202ccdb0f243469dab5576e71bf1352b2045df
Shaun Masterman <shaun@masterman.com>
2020-02-24 19:11:31 +0000

    Hmmm - trying to isolate queue creation problems

--- tracked-hashtag-service/src/main/kotlin/com/fourchimps/hashtag/RabbitMQConfiguration.kt
+++ tracked-hashtag-service/src/main/kotlin/com/fourchimps/hashtag/RabbitMQConfiguration.kt
@@ -3,6 +3,7 @@ package com.fourchimps.hashtag
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import mu.KotlinLogging
 import org.springframework.amqp.core.Binding
 import org.springframework.amqp.core.BindingBuilder
 import org.springframework.beans.factory.annotation.Value
@@ -12,25 +13,31 @@ import org.springframework.amqp.core.Queue
 import org.springframework.amqp.core.TopicExchange
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
 
-
 @Configuration
 class RabbitMQConfiguration(
         @Value("\${queue.twitter}") private val queue: String,
         @Value("\${exchange.twitter}") private val exchange: String,
         @Value("\${routing_key.track}") private val routingKey: String) {
 
+    private val logger = KotlinLogging.logger {}
+
     @Bean
     fun queue(): Queue {
+        logger.info { "Queue: $queue" }
+
         return Queue(this.queue, false)
     }
 
     @Bean
     fun exchange(): TopicExchange{
+        logger.info { "Exchange: $exchange "}
         return TopicExchange(this.exchange)
     }
 
     @Bean
     fun binding(queue: Queue, exchange: TopicExchange): Binding {
+        logger.info { "Binding with Key: $routingKey " }
+
         return BindingBuilder.bind(queue).to(exchange).with(this.routingKey)
     }
 
--- tweet-gathering/src/main/kotlin/com/fourchimps/gather/RabbitMQConfiguration.kt
+++ tweet-gathering/src/main/kotlin/com/fourchimps/gather/RabbitMQConfiguration.kt
@@ -3,14 +3,15 @@ package com.fourchimps.gather
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import mu.KotlinLogging
 import org.springframework.amqp.core.Binding
 import org.springframework.amqp.core.BindingBuilder
-import org.springframework.amqp.core.Queue
-import org.springframework.amqp.core.TopicExchange
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.amqp.core.Queue
+import org.springframework.amqp.core.TopicExchange
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
 
 @Configuration
 class RabbitMQConfiguration(
@@ -18,18 +19,25 @@ class RabbitMQConfiguration(
         @Value("\${exchange.twitter}") private val exchange: String,
         @Value("\${routing_key.track}") private val routingKey: String) {
 
+    private val logger = KotlinLogging.logger {}
+
     @Bean
     fun queue(): Queue {
+        logger.info { "Queue: $queue" }
+
         return Queue(this.queue, false)
     }
 
     @Bean
     fun exchange(): TopicExchange {
+        logger.info { "Exchange: $exchange "}
         return TopicExchange(this.exchange)
     }
 
     @Bean
     fun binding(queue: Queue, exchange: TopicExchange): Binding {
+        logger.info { "Binding with Key: $routingKey " }
+
         return BindingBuilder.bind(queue).to(exchange).with(this.routingKey)
     }
 
